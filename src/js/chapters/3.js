8d91fd85681c17c2809245d2f07a3748a1d1cf03
leithba <leithbenabdessalem@gmail.com>
2021-06-10 10:47:05 +0200

    :art: chap03 reverse animation fix + unique starship

--- src/js/chapters/3.js
+++ src/js/chapters/3.js
@@ -3,9 +3,11 @@ import Launcher from '../World/Launcher'
 import { Clock, Vector3 } from 'three'
 import gsap from 'gsap'
 import lerp from '../Tools/Lerp'
+import Starship from '../World/Starship'
 import Satellite from '../World/Satellite'
 
 let c = new Chapter(3)
+c.title = 'Docking Sequence'
 
 c.init = (options) => {
   c.camera = options.world.camera.camera
@@ -13,14 +15,22 @@ c.init = (options) => {
   c.mouse = c.world.mouse.mouse
   c.debug = options.debug
   c.world = options.world
-  c.starship = options.starship
+  c.starship = new Starship(options)
   c.satellite = new Satellite(options)
+  c.world.container.add(c.starship.container)
+
+
+  c.starship.container.visible = true
+  c.starship.container.children[0].children.forEach(e => {
+    e.visible = false
+  });
   c.world.container.add(c.satellite.container)
   c.freeViewTime = 0.3
   c.transTime = 0.05
   c.travelTime = 0.4
   c.cameraTarget = new Vector3(0, 0, 0)
   c.time = 0
+  c.objects.push(c.starship.container)
   c.objects.push(c.satellite.container)
   c.objects.forEach(object => {
     object.visible = false
@@ -29,6 +39,7 @@ c.init = (options) => {
   c.circle = document.getElementById('circle')
   c.lastCamPos = new Vector3(0, 0, 0)
   c.circlePos = new Vector3(0, 0, 0)
+
 }
 
 c.start = () => {
@@ -37,7 +48,6 @@ c.start = () => {
   c.controls.autoRotate = true
   c.camera.position.set(-30, 0, 0)
   c.starship.container.position.set(0, -0, -30)
-  console.log(c.satellite)
   c.satellite.container.scale.set(1, 1, 1)
   c.starship.container.scale.set(0.1, 0.1, 0.1)
   c.anchor = new Vector3(0, 0, 0)
@@ -46,15 +56,8 @@ c.start = () => {
   c.objects.forEach(object => {
     object.visible = true
   })
-  c.starship.container.visible = true
-  c.starship.container.children[0].children.forEach(e => {
-    e.visible = false
-  });
   c.starship.container.rotation.x = Math.PI / 2
   c.starship.container.rotation.y = -Math.PI / 2;
-
-  //c.starship.container.rotateX(Math.PI / 2)
-  //c.starship.container.rotateY(-Math.PI / 2)
   c.starship.container.children[0].children[13].visible = true
   c.world.scene.fog.far = 2000
 
@@ -65,7 +68,6 @@ c.start = () => {
 }
 
 c.update = () => {
-  console.log(c.progress)
   if (c.progress < c.freeViewTime) {
     c.controls.enabled = true
     c.controls.autoRotate = true
@@ -104,13 +106,27 @@ c.update = () => {
     )
     c.starship.container.position.lerpVectors(
       new Vector3(0, -0, -30),
-      new Vector3(-c.mouse.x * 5 * freedom, c.mouse.y * 5 * freedom, -25),
+      new Vector3(-c.mouse.x * 5 * freedom, c.mouse.y * 5 * freedom, -24.5),
       Math.sin((progress * Math.PI) / 2)
     )
     c.satellite.container.position.lerpVectors(
       new Vector3(-0, -0, -0 + 4),
       new Vector3(-0, -0, -0),
       Math.sin((progress * Math.PI) / 2))
+    return
+  }
+  if (c.progress < c.freeViewTime + c.transTime + c.travelTime + c.transTime) {
+    c.controls.enabled = false
+    c.controls.autoRotate = false
+    let transProgress = (c.progress - (c.freeViewTime + c.transTime + c.travelTime)) / c.transTime
+    c.camera.position.lerpVectors(
+      new Vector3(0, 3, -15),
+      c.lastCamPos,
+      -(Math.cos(Math.PI * transProgress) - 1) / 2
+    )
+
+    c.circle.style.visibility = 'hidden'
+    c.crosshair.style.visibility = 'hidden'
     return
   }
   c.lastCamPos = { ...c.camera.position }
