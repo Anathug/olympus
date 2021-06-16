490e3696b740a10e658e21ca9ef7d1e095fd37fe
BEN ABDESSALEM Leith <lbenabdessalem@g-a102b-10.local>
2021-06-16 13:07:06 +0200

    :sparkles: sound animated

--- src/js/chapters/2.js
+++ src/js/chapters/2.js
@@ -1,5 +1,8 @@
 import Chapter from '../Chapter'
-import { AnimationMixer, LoopOnce, AudioListener, Audio } from 'three'
+import { AnimationMixer, LoopOnce, LoopRepeat, AudioListener, Audio } from 'three'
+import { Howl, Howler } from 'howler';
+
+
 
 let c = new Chapter(2)
 
@@ -15,17 +18,22 @@ c.init = options => {
   createCams()
   createAnimation()
   c.hideObjects(c.objects)
+  c.audio = c.assets.sounds.chap02
 
-  c.listener = new AudioListener();
-  // c.objects.push(c.listener);
-  c.world.container.add(c.listener)
 
-  // create a global audio source
-  c.sounds = [new Audio(c.listener), new Audio(c.listener)];
+  // c.listener = new AudioListener();
+  // c.world.container.add(c.listener);
+  // c.sound = new Audio(c.listener);
+  // c.sound.setBuffer(c.assets.sounds.chap02);
 
-  // load a sound and set it as the Audio object's buffer
-  c.sounds[0].setBuffer(c.assets.sounds.chap02);
-  c.sounds[1].setBuffer(c.assets.sounds.chap02_r);
+
+  c.soundN = new Howl({
+    src: ['./sounds/chap02.mp3']
+  });
+  c.soundR = new Howl({
+    src: ['./sounds/chap02_r.mp3']
+  });
+  c.currentSound = c.soundN
 }
 
 c.start = () => {
@@ -33,26 +41,57 @@ c.start = () => {
   c.showObjects(c.objects)
   c.handler.allowScroll = true
   c.handler.autoScroll = true
+  c.duration = c.soundN.duration()
+  c.handler.setAutoScrollSpeed(c.duration)
   c.world.renderer.switchCam(c.cams[1])
-  c.sounds[0].play()
+  c.reversed = false
+  c.soundN.play();
+  c.soundR.stop();
 }
 
 c.update = () => {
-  c.mixer.setTime(c.progress * c.animationDuration)
-  console.log(c.sounds[0].context.currentTime, c.sounds[0].buffer.duration)
-  c.sounds[0].playbackRate = 0.2
+  c.mixer.setTime(Math.min(c.progress * c.duration, c.animationDuration))
+  let playbackRate = 1 + c.progress * c.duration - (c.reversed ? c.duration - c.soundR.seek() : c.soundN.seek())
 
+
+  if (playbackRate === 0) {
+    playbackRate = 0.0000000000001;
+  }
+
+  if (playbackRate > 0) {
+    if (c.reversed) { //was rev but no more
+      c.currentSound = c.soundN
+      c.soundN.play()
+      c.soundN.seek(c.duration - c.soundR.seek())
+      c.soundR.stop()
+      c.reversed = false
+    }
+    c.soundR.seek(c.duration - c.soundN.seek())
+  } else {
+    if (!c.reversed) { //was not rev but now is
+      c.currentSound = c.soundR
+      c.soundR.play()
+      c.soundR.seek(c.duration - c.soundN.seek())
+      c.soundN.stop()
+      c.reversed = true
+    }
+    c.soundN.seek(c.duration - c.soundR.seek())
+  }
+
+  c.currentSound.rate(Math.abs(playbackRate))
 }
+
+// c.wheel = () => {
+//   c.sound.stop()
+//   c.sound.seek(c.progress * c.sound.duration())
+//   c.sound.play()
+// }
 
 c.end = () => {
   c.hideChapter('chapter_2')
   c.hideObjects(c.objects)
   c.allowScroll = false
   c.world.renderer.switchCam('default')
-
-
-  c.sounds[0].stop()
-  c.sounds[1].stop()
 }
 
 const createCams = () => {
@@ -72,7 +111,7 @@ const createAnimation = () => {
   c.mixer = new AnimationMixer(c.gltf.scene)
   c.animationDuration = 0
   clips.forEach(clip => {
-    c.mixer.clipAction(clip).setLoop(LoopOnce)
+    c.mixer.clipAction(clip).setLoop(LoopRepeat)
     c.mixer.clipAction(clip).reset().play();
     if (clip.duration > c.animationDuration)
       c.animationDuration = clip.duration
